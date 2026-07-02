74b9071019e4beea56e380a5cb99e9ff10752b33
Arocadev <aleexrc.xtv@gmail.com>
2026-07-02 23:25:36 +0200

    feat: añade modelo de recordatorios y soporte Hive

--- lib/models/recordatorio.dart
+++ lib/models/recordatorio.dart
@@ -16,10 +16,22 @@ class Recordatorio extends HiveObject {
   @HiveField(3)
   bool completado;
 
+  @HiveField(4)
+  bool avisarDiaAntes;
+
+  @HiveField(5)
+  bool avisar6HorasAntes;
+
+  @HiveField(6)
+  bool avisar1HoraAntes;
+
   Recordatorio({
     required this.titulo,
     required this.descripcion,
     required this.fecha,
     this.completado = false,
+    this.avisarDiaAntes = true,
+    this.avisar6HorasAntes = false,
+    this.avisar1HoraAntes = false,
   });
 }
--- lib/models/recordatorio.g.dart
+++ lib/models/recordatorio.g.dart
@@ -21,13 +21,16 @@ class RecordatorioAdapter extends TypeAdapter<Recordatorio> {
       descripcion: fields[1] as String,
       fecha: fields[2] as DateTime,
       completado: fields[3] as bool,
+      avisarDiaAntes: fields[4] as bool,
+      avisar6HorasAntes: fields[5] as bool,
+      avisar1HoraAntes: fields[6] as bool,
     );
   }
 
   @override
   void write(BinaryWriter writer, Recordatorio obj) {
     writer
-      ..writeByte(4)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.titulo)
       ..writeByte(1)
@@ -35,7 +38,13 @@ class RecordatorioAdapter extends TypeAdapter<Recordatorio> {
       ..writeByte(2)
       ..write(obj.fecha)
       ..writeByte(3)
-      ..write(obj.completado);
+      ..write(obj.completado)
+      ..writeByte(4)
+      ..write(obj.avisarDiaAntes)
+      ..writeByte(5)
+      ..write(obj.avisar6HorasAntes)
+      ..writeByte(6)
+      ..write(obj.avisar1HoraAntes);
   }
 
   @override
